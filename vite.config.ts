4ffb2bb13d0c8f3b013099bad5fe326b9e8697c2
Илья Копендох <ilakopendoh@MacBook-Air-Ila.local>
2024-09-02 15:19:04 +0300

    host instal

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000,
+    host: '0.0.0.0', 
   },
   resolve: {
     alias: {
